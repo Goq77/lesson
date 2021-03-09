0720cd4f0b1a968459805ad2dcb0aa825a302d5f
Goq77 <gh.goq89@gmail.com>
2021-03-09 21:38:49 +0400

    lesson 16

---
+++ home6.py
@@ -0,0 +1,11 @@
+import math
+
+# a = int(input('a: '))
+# b = int(input('b: '))
+# x = (((a+b)/2)**0.5) + pow((((a+b)**3)/math.factorial(a))**3, 1/3)
+# print(x)
+
+
+n = input('n: ')
+res = int(n)+ int(n*2) + int(n*3)
+print(res)
